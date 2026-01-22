3492ffa0a0ae35e0435ba7a4fef802383a4f3821
sugandahabdy11-ui <suganda.habdy11@gmail.com>
2026-01-22 23:37:16 +0700

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,4 @@
-import { Layout } from "@/components/Layout";
+import { Layout } from "@/components/layouts/MainLayout";
 import { Link } from "react-router-dom";
 import { Calculator, Home, ArrowRight, Shield, Zap, TrendingUp } from "lucide-react";
 import { Button } from "@/components/ui/button";
