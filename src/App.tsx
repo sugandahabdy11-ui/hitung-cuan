829dec00e47abe215a2522ce2397793eed318850
sugandahabdy11-ui <suganda.habdy11@gmail.com>
2026-01-22 17:12:58 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
-import { Footer } from "@/components/Footer";
 
 // Pages
 import Index from "./pages/Index";
